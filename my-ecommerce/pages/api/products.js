cfec20f33a8cfec73a255daa41fb4ef03f0c0c38
Maznu007 <hasnatrafiuddinmunif@gmail.com>
2026-02-05 14:29:38 +0600

    components

--- my-ecommerce/pages/api/products.js
+++ my-ecommerce/pages/api/products.js
@@ -1,6 +1,8 @@
-import {initMongoose} from "../../lib/mongoose"
-import Product from "../../models/product"
-export default async function handle(req,res) {
-    await initMongoose();
-    res.json( await Product.find().exec());
+import { initMongoose } from "../../lib/mongoose";
+import Product from "../../models/Product";
+
+export default async function handle(req, res) {
+  await initMongoose();
+  const products = await Product.find().exec();
+  res.json(products);
 }
--- my-ecommerce/pages/index.js
+++ my-ecommerce/pages/index.js
@@ -1,23 +1,40 @@
+import { useEffect, useState } from "react";
+import Product from "../components/product";
 
 export default function Home() {
+  const [productsInfo, setProductsInfo] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/products")
+      .then((res) => res.json())
+      .then((json) => setProductsInfo(json));
+  }, []);
+
+  const categoriesNames = [
+    ...new Set(productsInfo.map((p) => p.category)),
+  ];
+
   return (
-  <div className ="p-5">
-    <div>
-      <h2 className="text-2xl font-bold">Mobile</h2>
-    <div className="w-64">
-    <div className="bg-blue-100 p-5 rounded-xl">
-      <img src="/products/iphone.png" alt="Mobile"/>
+    <div className="p-5">
+      {categoriesNames.map((categoryName) => (
+        <div key={categoryName} className="mb-10">
+          <h2 className="text-2xl font-bold capitalize mb-4">
+            {categoryName}
+          </h2>
+
+          <div className="flex gap-4 flex-wrap overflow-x-scroll snap-x scrollbar-hide">
+            {productsInfo
+              .filter((p) => p.category === categoryName)
+              .map((productInfo) => (
+                <Product
+                  key={productInfo._id}
+                  className="px-20"
+                  {...productInfo}
+                />
+              ))}
+          </div>
+        </div>
+      ))}
     </div>
-    <div className="mt-2"> 
-      <h3 className="font-bold text-lg">Iphone 14 pro</h3>
-    </div>
-    <p className="text-sm mt-1 leading-4">The latest iPhone with advanced features and a stunning display.iphone 14 pro max</p>
-    <div className="flex mt-1">
-      <div className="text-2xl font-bold grow">899$</div>
-      <button className="bg-emerald-400 text-white py-1 px-3 rounded-xl">+</button>
-    </div>
-    </div>
-    </div>
-  </div>
-  )
+  );
 }
